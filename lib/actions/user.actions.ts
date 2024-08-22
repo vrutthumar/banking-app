75529359edd354812999388bf861f3966685edb2
vrutthumar <vrutthumar24@gmail.com>
2024-08-22 18:05:01 +0530

    Created UI for auth

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use server';
+
+export const signIn = async () => {
+        try {
+                // Mutation / Database
+        } catch (error) {
+                console.log(error);
+        }
+};
+
+export const signUp = async (userData : SignUpParams) => {
+        try {
+                // Mutation / Database
+        } catch (error) {
+                console.log(error);
+        }
+};
